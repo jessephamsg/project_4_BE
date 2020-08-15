0199fcc661d8b923f697c38e9c0c67a8f679998b
YJ Lee <leeyuejia@gmail.com>
2020-08-15 18:25:18 +0800

    doing authentication layer

--- server.js
+++ server.js
@@ -3,14 +3,30 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const user= []
+
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false }));
 // Routes
 app.get('/', ( req, res )=>{
 //   console.log(`Get request from /, sent response.`);
 //   console.log(req.params);
 //   console.log(req.query);
-  res.send('Project 4 BE');
+  // res.send('Project 4 BE');
+  res.render('index.ejs', {name:'username'})
 });
 
+app.get('/login', (req,res) => {
+  res.render('login.ejs')
+})
+
+app.get('/register', (req,res) => {
+  res.render('register.ejs')
+})
+
+app.post('/register', (req,res) => {
+
+})
 // App Listen at the last
 app.listen(PORT, ()=> {
 console.log(`Project 4 BE : I am listening on port: ${PORT}`);});
