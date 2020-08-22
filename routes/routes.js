771003e47a6edaf6547f303db54c74a835515704
YJ Lee <leeyuejia@gmail.com>
2020-08-22 10:19:46 +0800

    ensure authentication service done

--- routes/routes.js
+++ routes/routes.js
@@ -31,8 +31,6 @@ router.post('/games', gameControllers.createOneGame);
 router.put('/games/:idx', gameControllers.updateOneGame);
 router.delete('/games/:idx', gameControllers.deleteOneGame);
 
-router.get('/success', (req,res) => {res.send(`success with`, req.user)}); // trying to redirect this route upon successful login
-router.get('/unsuccess', (req,res) => {res.send(`unsuccess with${messages.error}`)}); // trying to redirect this route upon unsuccessful login
 router.post('/login', parentControllers.login); // logging in with user input with username and password
 
 module.exports = router;
