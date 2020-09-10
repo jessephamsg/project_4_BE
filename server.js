20a8c9bf391960089a6a6fa2efa438345f6e0321
YJ Lee <leeyuejia@gmail.com>
2020-09-10 18:44:51 +0800

    added multiple url in CORS

--- server.js
+++ server.js
@@ -18,22 +18,28 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // auth middleware
-app.use(
-    cors({
-      origin: [frontEndUrl, frontEndUrl + '/games', 'http://localhost:8000'],
-      credentials: true,
-      'Access-Control-Allow-Credentials': true, // trying this
-      methods: 'GET, PUT, POST, DELETE'
-    })
-  );
+// app.use(
+//     cors({
+//       origin: [frontEndUrl, frontEndUrl + '/games', 'http://localhost:8000'],
+//       credentials: true,
+//       'Access-Control-Allow-Credentials': true, // trying this
+//       methods: 'GET, PUT, POST, DELETE'
+//     })
+//   );
 
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Credentials", "true");
-//     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS, PATCH");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, x-access-token, Cookie, Content-Type, access_token, Accept");
-//     next();
-// });
+app.use(function(req, res, next) {
+    const allowedOrigin = [frontEndUrl, frontEndUrl + '/games', 'http://localhost:8000']
+    const origin = req.header.origin;
+    if (allowedOrigin.includes(origin)) {
+        res.setHeader("Access-Control-Allow-Origin", origin)
+    }
+    
+    // res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS, PATCH");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, x-access-token, Cookie, Content-Type, access_token, Accept");
+    next();
+});
 app.set('trust proxy', 1)
 app.use(session ({
     secret: process.env.SESSION_SECRET || 'secretly',
