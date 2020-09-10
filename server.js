78c9713d1225b64345d7544f402e9e011f558391
YJ Lee <leeyuejia@gmail.com>
2020-09-10 16:50:56 +0800

    buggy

--- server.js
+++ server.js
@@ -18,31 +18,32 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // auth middleware
-app.use(
-    cors({
-      origin: [frontEndUrl, 'http://localhost:3000'],
-      credentials: true,
-      'Access-Control-Allow-Credentials': true, // trying this
-      methods: 'GET, PUT, POST, DELETE'
-    })
-  );
-  
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", frontEndUrl);
-//     res.header("Access-Control-Allow-Credentials", "true");
-//     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS, PATCH");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, x-access-token, Cookie, Content-Type, access_token, Accept");
-//     next();
-// });
+
+// app.use(
+//     cors({
+//       origin: [frontEndUrl, 'http://localhost:3000'],
+//       credentials: true,
+//       'Access-Control-Allow-Credentials': true, // trying this
+//       methods: 'GET, PUT, POST, DELETE'
+//     })
+//   );
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", frontEndUrl);
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS, PATCH");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, x-access-token, Cookie, Content-Type, access_token, Accept");
+    next();
+});
 app.set('trust proxy', 1)
 app.use(session ({
     secret: process.env.SESSION_SECRET || 'secretly',
-    resave : true, // should we resave session variable if nothing has changed ?
+    resave : false, // should we resave session variable if nothing has changed ?
     saveUninitialized : true, // should we save empty value in session ?
     cookie: {
         sameSite : 'none',
         secure : true
-    }
+    },
 }))
 
 initializePassport(passport)
